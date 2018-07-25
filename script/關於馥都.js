3226c5555e81404f96638c94655c0f5273c92f8f
stevenweng93 <41528386+stevenweng93@users.noreply.github.com>
2018-07-25 11:56:02 -0700

    New Fudu

---
+++ script/關於馥都.js
@@ -0,0 +1,10 @@
+// $(document).ready(function(){
+
+//     //Check to see if the window is top if not then display button
+//     $(window).scroll(function(){
+//         if ($(this).scrollTop() > 50) {
+//             $('#section01').fadeOut();
+//         } else {
+//             $('#section01').fadeIn();
+//         }
+//     });
