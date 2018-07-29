45b9bcf93bd47f99f9ee65a1970958dfed4a612c
stevenweng93 <41528386+stevenweng93@users.noreply.github.com>
2018-07-29 14:36:12 -0700

    more pictures

--- script/關於馥都.js
+++ script/關於馥都.js
@@ -1,14 +1,3 @@
-// $(document).ready(function(){
-
-//     //Check to see if the window is top if not then display button
-//     $(window).scroll(function(){
-//         if ($(this).scrollTop() > 50) {
-//             $('#section01').fadeOut();
-//         } else {
-//             $('#section01').fadeIn();
-//         }
-//     });
-
 $(window).scroll(function() {
         scrollPosition = $(window).scrollTop();
         if(scrollPosition >= 80) {
@@ -30,4 +19,14 @@ $(".col-lg").hover(function() {
         var this2 = $(this).attr("id") + "-2";
         $("."+this1).removeClass("box-shadow");
         $("."+this2).removeClass("bottom-border");
+      });
+
+$(".相片hover").hover(function() {
+        var thisPhoto = $(this).attr("id");
+        $("."+thisPhoto).addClass("相片opacity");
+        $(".相片hover").not(this).stop(true,true).addClass("相片lowOpacity");
+      }, function() {
+        var thisPhoto = $(this).attr("id");
+        $("."+thisPhoto).removeClass("相片opacity");
+        $(".相片hover").not(this).stop(true,true).removeClass("相片lowOpacity");
       });
